34fd3aca61e8987191b2a7816680ba06d1081997
RydelMorgan <pheiterer@hotmail.com>
2022-03-24 20:49:56 -0300

    Aula7
    >.<

---
+++ src/rydelmorgan/maratonajava/introdução/Aula05Condicionais.java
@@ -0,0 +1,10 @@
+package rydelmorgan.maratonajava.introdução;
+
+public class Aula05Condicionais {
+    public static void main(String[] args) {
+        double salario = 6000;
+
+        String resultado = salario>5000 ? "Eu vou doar 500 pro DevDojo":"Ainda não tenho condições, mas vou ter";
+        System.out.println(resultado);
+    }
+}
---
+++ src/rydelmorgan/maratonajava/introdução/Aula07Array.java
@@ -0,0 +1,9 @@
+package rydelmorgan.maratonajava.introdução;
+
+public class Aula07Array {
+    public static void main(String[] args) {
+        int[] idades= new int[3];
+        idades[0] = 21;
+        System.out.println(idades[0]);
+    }
+}
