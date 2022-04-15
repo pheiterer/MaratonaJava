f0d4a53fcad1e779fa1c6b84f9a68968f6d2d8c6
RydelMorgan <pheiterer@hotmail.com>
2022-04-15 13:27:07 -0300

    Wrapper
    learning how to use wrapper

---
+++ src/rydelmorgan/maratonajava/javacore/Pwrapper/test/WrapperTest01.java
@@ -0,0 +1,24 @@
+package rydelmorgan.maratonajava.javacore.Pwrapper.test;
+
+import java.util.List;
+
+public class WrapperTest01 {
+    byte byteP = 1;
+    short shortP = 1;
+    int intP = 1;
+    long longP = 10L;
+    float floatP = 10F;
+    double doubleP = 100;
+    char charP = 'W';
+    boolean bollearP = false;
+
+    Byte byteW = 127;
+    Short shortW = 1;
+    Integer intW = 1;
+    Long longW = 10L;
+    Float floatW = 10F;
+    Double doubleW = 10D;
+    Character charW = 'W';
+    Boolean bollearW = false;
+    
+}
