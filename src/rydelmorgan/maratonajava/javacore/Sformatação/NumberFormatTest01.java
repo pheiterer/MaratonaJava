1fa25cc04bde9161df901fdc28c65fa8e9d7d6de
RydelMorgan <pheiterer@hotmail.com>
2022-04-16 20:14:15 -0300

    Update NumberFormatTest01.java
    parse and locale

--- src/rydelmorgan/maratonajava/javacore/Sformatação/NumberFormatTest01.java
+++ src/rydelmorgan/maratonajava/javacore/Sformatação/NumberFormatTest01.java
@@ -1,6 +1,7 @@
 package rydelmorgan.maratonajava.javacore.Sformatação;
 
 import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Locale;
 
 public class NumberFormatTest01 {
@@ -8,16 +9,21 @@ public class NumberFormatTest01 {
         Locale locale0 = new Locale("pt", "BR");
         Locale locale = Locale.JAPAN;
         Locale locale1 = Locale.ITALY;
-        NumberFormat[] numberFormats = new NumberFormat[4];
-        numberFormats[0] = NumberFormat.getInstance(locale);
-        numberFormats[1] = NumberFormat.getInstance(locale1);
-        numberFormats[2] = NumberFormat.getInstance(locale0);
-        numberFormats[3] = NumberFormat.getInstance();
-        double valor = 100000000.2130;
-        for (NumberFormat numberFormat : numberFormats) {
+        NumberFormat[] nfa = new NumberFormat[4];
+        nfa[3] = NumberFormat.getCurrencyInstance(locale);
+        nfa[1] = NumberFormat.getCurrencyInstance(locale1);
+        nfa[2] = NumberFormat.getCurrencyInstance(locale0);
+        nfa[0] = NumberFormat.getInstance();
+        double valor = 1_000.2130;
+        for (NumberFormat numberFormat : nfa) {
             System.out.println(numberFormat.format(valor));
         }
-
+        String valorString ="1_000.2130";
+        try {
+            System.out.println(nfa[0].parse(valorString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
     }
 }
