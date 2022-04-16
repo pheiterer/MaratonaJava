9f9d048a552dd4415b2dc129f6ea95e846103c3f
RydelMorgan <pheiterer@hotmail.com>
2022-04-16 18:58:33 -0300

    DateFormat
    maybe one day i use this, btw now i know what is.

---
+++ src/rydelmorgan/maratonajava/javacore/Rdates/test/DateTest01.java
@@ -0,0 +1,12 @@
+package rydelmorgan.maratonajava.javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date();// long 100000
+        date.setTime(date.getTime() + 3600000);
+        System.out.println(date);
+
+    }
+}
