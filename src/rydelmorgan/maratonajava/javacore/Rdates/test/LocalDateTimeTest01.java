0b670a1626e485c8e233bc179be07a36093b1b2c
RydelMorgan <pheiterer@hotmail.com>
2022-04-17 11:55:07 -0300

    Create LocalDateTimeTest01.java
    using localDateTime alone and with localDate and localTime

---
+++ src/rydelmorgan/maratonajava/javacore/Rdates/test/LocalDateTimeTest01.java
@@ -0,0 +1,23 @@
+package rydelmorgan.maratonajava.javacore.Rdates.test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+
+public class LocalDateTimeTest01 {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDate date = LocalDate.of(2022, Month.AUGUST, 21);
+        LocalTime time = LocalTime.parse("09:45:00");
+        System.out.println(localDateTime);
+        System.out.println(date);
+        System.out.println(time);
+        LocalDateTime ldt1 = date.atTime(time);
+        LocalDateTime ldt2 = time.atDate(date);
+        System.out.println(ldt1);
+        System.out.println(ldt2);
+
+    }
+}
