636017087e3b869e645711acb370a61a1f6ee0a6
RydelMorgan <pheiterer@hotmail.com>
2022-04-17 19:43:37 -0300

    emails matcher
    using emails

--- src/rydelmorgan/maratonajava/javacore/Uregex/test/PatternMatcherTest04.java
+++ src/rydelmorgan/maratonajava/javacore/Uregex/test/PatternMatcherTest04.java
@@ -3,7 +3,7 @@ package rydelmorgan.maratonajava.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest03 {
+public class PatternMatcherTest04 {
     public static void main(String[] args) {
         // \d = Todos os dígitos
         // \D = Tudo que não for dígito
@@ -20,8 +20,9 @@ public class PatternMatcherTest03 {
         // | o(v|c)o  ovo | oco
         // $
         // . entre
-        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
-        String texto = "12 0x 0X 0xFFABC 0x10G 0x1";
+        String regex = "([a-zA-Z0-9\\._-])+@([a-zA-Z])+(\\.([a-zA-Z])+)*";
+        String texto = "pheiterer@hotmail.com, luffy@hotmail.com, @#!@zoro@mail.br, teste@gmail.com.br, sakura@mail";
+        System.out.println(texto.split(",")[1].trim());
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto);
         System.out.println("texto: " + texto);
