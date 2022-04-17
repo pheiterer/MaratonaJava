3b684613d8ceca958e0c7943eb589bc21735e535
RydelMorgan <pheiterer@hotmail.com>
2022-04-17 13:08:24 -0300

    Create TemporalAdjustTest01.java
    time determinated

---
+++ src/rydelmorgan/maratonajava/javacore/Rdates/test/TemporalAdjustTest01.java
@@ -0,0 +1,26 @@
+package rydelmorgan.maratonajava.javacore.Rdates.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+public class TemporalAdjustTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        now = now.with(ChronoField.DAY_OF_MONTH, 18);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.THURSDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.THURSDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+        
+    }
+}
