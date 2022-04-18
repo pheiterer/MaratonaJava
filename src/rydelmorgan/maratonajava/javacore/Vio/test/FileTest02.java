6e74a6694265963270b9979c9dee1599efcb1aef
RydelMorgan <pheiterer@hotmail.com>
2022-04-17 21:53:29 -0300

    prompt windows
    its like prompt of windows 😒

---
+++ src/rydelmorgan/maratonajava/javacore/Vio/test/FileTest02.java
@@ -0,0 +1,20 @@
+package rydelmorgan.maratonajava.javacore.Vio.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileTest02 {
+    public static void main(String[] args) throws IOException {
+        File fd = new File("arquivo");
+        boolean isDiretorioCriado = fd.mkdir();
+        System.out.println(isDiretorioCriado);
+        File newFile = new File(fd, "texto.txt");
+        boolean isFileCreated = newFile.createNewFile();
+        System.out.println(isFileCreated);
+
+        File fileRenamed = new File(fd, "arquivo_renamed.txt");
+        boolean isRenamed = newFile.renameTo(fileRenamed);
+        System.out.println(isRenamed);
+        
+    }
+}
