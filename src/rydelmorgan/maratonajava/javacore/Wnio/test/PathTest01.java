29908790bd6301aa6263402057bf8ab62ee36eb5
RydelMorgan <pheiterer@hotmail.com>
2022-04-17 22:16:58 -0300

    Paths and Files

--- src/rydelmorgan/maratonajava/javacore/Wnio/test/PathTest01.java
+++ src/rydelmorgan/maratonajava/javacore/Wnio/test/PathTest01.java
@@ -3,7 +3,7 @@ package rydelmorgan.maratonajava.javacore.Wnio.test;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class PathTEst01 {
+public class PathTest01 {
     public static void main(String[] args) {
         Path p1 = Paths.get("C:\\Users\\pheit\\OneDrive\\Documentos\\GitHub\\Maratona\\file.txt");
         Path p2 = Paths.get("C:\\Users\\pheit\\OneDrive\\Documentos\\GitHub\\Maratona", "file.txt");
