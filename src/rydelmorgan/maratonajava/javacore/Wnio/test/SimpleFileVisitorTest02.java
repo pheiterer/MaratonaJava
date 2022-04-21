35d89769847a42f20572da9ce5e60fafbb2f13e0
RydelMorgan <pheiterer@hotmail.com>
2022-04-20 21:27:58 -0300

    SimpleVisitor
    using other methods to see files or directorys

--- src/rydelmorgan/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest02.java
+++ src/rydelmorgan/maratonajava/javacore/Wnio/test/SimpleFileVisitorTest02.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
-class ListAllFilesJAva extends SimpleFileVisitor<Path> {
-    protected ListAllFilesJAva() {
+class ListAllFiles extends SimpleFileVisitor<Path> {
+    protected ListAllFiles() {
         super();
     }
 
     @Override
-    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-        return super.preVisitDirectory(dir, attrs);
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs){
+        return  FileVisitResult.CONTINUE;
     }
 
     @Override
@@ -21,21 +21,19 @@ class ListAllFilesJAva extends SimpleFileVisitor<Path> {
 
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-        return super.postVisitDirectory(dir, exc);
+        return  FileVisitResult.CONTINUE;
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs){
-        if (file.getFileName().toString().endsWith(".java")) {
-            System.out.println(file.getFileName());
-        }
-        return FileVisitResult.CONTINUE;
+        System.out.println(file.getFileName());
+        return  FileVisitResult.CONTINUE;
     }
 }
 
-public class SimpleFileVisitorTest01 {
+public class SimpleFileVisitorTest02 {
     public static void main(String[] args) throws IOException {
-        Path root = Paths.get(".");
-        Files.walkFileTree(root, new ListAllFilesJAva());
+        Path root = Paths.get("pasta");
+        Files.walkFileTree(root, new ListAllFiles());
     }
 }
