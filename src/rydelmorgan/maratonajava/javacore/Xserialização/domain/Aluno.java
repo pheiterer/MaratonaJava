c3569bcd0ca8df90bc9b775f275ab524d1b024b5
RydelMorgan <pheiterer@hotmail.com>
2022-04-21 13:59:13 -0300

    Serealizeble
    seralização e deseralização

---
+++ src/rydelmorgan/maratonajava/javacore/Xserialização/domain/Aluno.java
@@ -0,0 +1,48 @@
+package rydelmorgan.maratonajava.javacore.Xserialização.domain;
+
+import java.io.Serializable;
+
+public class Aluno implements Serializable {
+    private Long id;
+    private String name;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public Aluno(Long id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
