97c3fdd7435897d3de71cb852fae4a2b838238a0
RydelMorgan <pheiterer@hotmail.com>
2022-04-23 19:52:42 -0300

    Create ListTest01.java
    Using ArrayList and diamond to specify a type of list

---
+++ src/rydelmorgan/maratonajava/javacore/Ycolletions/test/ListTest01.java
@@ -0,0 +1,20 @@
+package rydelmorgan.maratonajava.javacore.Ycolletions.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest01 {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(300);
+        names.add("Paulo");
+        names.add("DevDojo Academy");
+
+        for (Object nome: names){
+            System.out.println(nome);
+        }
+        names.add("Suane");
+        for (int i = 0; i < names.size(); i++) {
+            System.out.println(names.get(i));
+        }
+    }
+}
