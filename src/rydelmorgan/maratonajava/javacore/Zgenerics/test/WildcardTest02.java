2ef6ae1d75002add3b9a022fad052f46700c67cd
RydelMorgan <pheiterer@hotmail.com>
2022-07-06 19:37:56 -0300

    Update WildcardTest02.java
    How to add a object in a List by Wildcard

--- src/rydelmorgan/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
+++ src/rydelmorgan/maratonajava/javacore/Zgenerics/test/WildcardTest02.java
@@ -6,12 +6,17 @@ public class WildcardTest02 {
     public static void main(String[] args) {
         List<Dog> dogs = List.of(new Dog(), new Dog());
         List<Cat> cats = List.of(new Cat(), new Cat());
+        printConsulta(dogs);
+        printConsulta(cats);
     }
-    //Type erasurej
-    private static void printConsulta(List<Animal> animals){
+    //Type erasure
+    private static void printConsulta(List<? extends Animal> animals){
         for (Animal animal : animals) {
             animal.consulta();
         }
 
+    }
+    private static void printConsultaAnimal(List<? super Animal> animals){
+         animals.add(new Dog());
     }
 }
