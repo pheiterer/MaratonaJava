84a9359302fb26c1207e4af2c9291e6e48811ec5
RydelMorgan <pheiterer@hotmail.com>
2022-07-18 20:57:42 -0300

    Stream

--- src/rydelmorgan/maratonajava/javacore/ZZEStream/domain/LightNovel.java
+++ src/rydelmorgan/maratonajava/javacore/ZZEStream/domain/LightNovel.java
@@ -1,5 +1,7 @@
 package rydelmorgan.maratonajava.javacore.ZZEStream.domain;
 
+import java.util.Objects;
+
 public class LightNovel {
     private String title;
     private double price;
@@ -7,6 +9,19 @@ public class LightNovel {
     public LightNovel(String title, double price) {
         this.title = title;
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LightNovel that = (LightNovel) o;
+        return Double.compare(that.price, price) == 0 && Objects.equals(title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, price);
     }
 
     public String getTitle() {
