a7f7ab3f2667c07e090fcdd5d3c55b966610fe98
RydelMorgan <pheiterer@hotmail.com>
2022-07-25 21:27:51 -0300

    Mais Stream

--- src/rydelmorgan/maratonajava/javacore/ZZEStream/test/StreamTest15.java
+++ src/rydelmorgan/maratonajava/javacore/ZZEStream/test/StreamTest15.java
@@ -4,24 +4,26 @@ import rydelmorgan.maratonajava.javacore.ZZEStream.domain.Category;
 import rydelmorgan.maratonajava.javacore.ZZEStream.domain.LightNovel;
 import rydelmorgan.maratonajava.javacore.ZZEStream.domain.Promotion;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.*;
 
-public class StreamTest13 {
+public class StreamTest15 {
     private static List<LightNovel> lightNovels = new ArrayList<>(List.of(new LightNovel("Tensei Shittara", 8.99, Category.FANTASY), new LightNovel("Overlord", 5.99, Category.FANTASY), new LightNovel("Violet Evergarden", 3.99, Category.DRAMA), new LightNovel("No Game no Life", 2.99, Category.FANTASY), new LightNovel("Sword Art Online", 15.99, Category.ROMANCE), new LightNovel("Fullmetal Alchemist", 5.99, Category.FANTASY), new LightNovel("Kumo desuga", 1.99, Category.FANTASY), new LightNovel("Kumo desuga", 1.99, Category.FANTASY), new LightNovel("Monogatari", 4, Category.ROMANCE)));
 
     public static void main(String[] args) {
-        Map<Promotion, List<LightNovel>> promotionListMap = lightNovels.stream().collect(groupingBy(ln -> getPromotion(ln)));
-        System.out.println(promotionListMap);
-        Map<Category, Map<Promotion, List<LightNovel>>> collect = lightNovels.stream().collect(groupingBy(LightNovel::getCategory,
-                groupingBy(StreamTest13::getPromotion)));
+        Map<Category, DoubleSummaryStatistics> collect = lightNovels.stream().collect(groupingBy(LightNovel::getCategory, Collectors.summarizingDouble(LightNovel::getPrice)));
         System.out.println(collect);
-    }
 
+        Map<Category, Set<Promotion>> collect1 = lightNovels.stream().collect(groupingBy(LightNovel::getCategory, mapping(StreamTest15::getPromotion, toSet())));
+        System.out.println(collect1);
+
+        Map<Category, LinkedHashSet<Promotion>> collect2 = lightNovels.stream().collect(groupingBy(LightNovel::getCategory, mapping(StreamTest15::getPromotion, toCollection(LinkedHashSet::new))));
+        System.out.println(collect2);
+    }
     private static Promotion getPromotion(LightNovel ln) {
         return ln.getPrice() < 6 ? Promotion.UNDER_PROMOTION : Promotion.NORMAL_PRICE;
     }
