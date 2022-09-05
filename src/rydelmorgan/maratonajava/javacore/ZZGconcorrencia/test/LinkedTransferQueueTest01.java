f5e054fcfb78a603a1b0885b0a46904ce8c3dea6
RydelMorgan <pheiterer@hotmail.com>
2022-09-05 18:27:10 -0300

    Create LinkedTransferQueueTest01.java

---
+++ src/rydelmorgan/maratonajava/javacore/ZZGconcorrencia/test/LinkedTransferQueueTest01.java
@@ -0,0 +1,25 @@
+package rydelmorgan.maratonajava.javacore.ZZGconcorrencia.test;
+
+import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TransferQueue;
+
+public class LinkedTransferQueueTest01 {
+    public static void main(String[] args) throws InterruptedException {
+        TransferQueue<Object> tq = new LinkedTransferQueue<>();
+        System.out.println(tq.add("william"));
+        System.out.println(tq.offer("william"));
+        System.out.println(tq.offer("william", 10, TimeUnit.SECONDS));
+        tq.put("Dev Dojo");
+        if(tq.hasWaitingConsumer()){
+            tq.transfer("DevDojo");
+        }
+        System.out.println(tq.tryTransfer("Paulo"));
+        System.out.println(tq.element());
+        System.out.println(tq.peek());
+        System.out.println(tq.poll());
+        System.out.println(tq.remove());
+        System.out.println(tq.take());
+        System.out.println(tq.remainingCapacity());
+    }
+}
