15ee473fe80fa1f9c3a624b371bc2926e43bf08a
Paulo Henrique dos Santos Eiterer <pheiterer@hotmail.com>
2023-04-11 19:39:11 -0300

    Docker and Maven:
    Add a dependencie in pom.xml and starting a connection with ConnectionFactory

--- src/main/java/rydelmorgan/maratonajava/javacore/ZZIJDBC/conn/ConnectionFactory.java
+++ src/main/java/rydelmorgan/maratonajava/javacore/ZZIJDBC/conn/ConnectionFactory.java
@@ -1,5 +1,20 @@
 package rydelmorgan.maratonajava.javacore.ZZIJDBC.conn;
 
-public class ConnectionFactory {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionFactory {
+    public static Connection getConnection(){
+        String url = "jdbc:mysql://localhost:3306/anime_store";
+        String username = "root";
+        String password = "root";
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 }
---
+++ src/main/java/rydelmorgan/maratonajava/javacore/ZZIJDBC/test/ConnectionfactoryTest.java
@@ -0,0 +1,9 @@
+package rydelmorgan.maratonajava.javacore.ZZIJDBC.test;
+
+import rydelmorgan.maratonajava.javacore.ZZIJDBC.conn.ConnectionFactory;
+
+public class ConnectionfactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
